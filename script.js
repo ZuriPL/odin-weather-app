6209b7ada26d958c2acf88ab0b8b9b0885b6d0e9
zuriii <michal.bak222@gmail.com>
2022-03-20 18:07:38 +0100

    1

--- script.js
+++ script.js
@@ -3,15 +3,26 @@ const date = document.querySelector('.date')
 
 async function getData(location) {
     const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=ed6f372defa1e8bf947ffa9b8e0fea17`
+        `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=ed6f372defa1e8bf947ffa9b8e0fea17&units=metric`
     )
     return await response.json()
 }
-function useData(x) {
-    console.log(x)
+function useData(data) {
+    console.log(data)
+    try {
+        document.querySelector('.wrapper.place-wrapper > p:first-of-type').textContent = data.name
+        document.querySelector('.wrapper.place-wrapper > p:last-of-type').textContent = data.sys.country
+        document.querySelector('#weather-type').textContent = data.weather[0].description
+        document.querySelector('.degs.main-temp').textContent = data.main.temp
+        document.querySelector('.degs.feel-temp').textContent = data.main['feels_like']
+        document.querySelector('.wind-speed-value.vel').textContent = data.wind.speed
+        document.querySelector('.humidity-value.percents').textContent = data.main.humidity
+    } catch (e) {
+        console.log('error')
+    }
 }
 
-getData('London').then(res => useData(res))
+getData('Warsaw').then(res => useData(res))
 
 async function startClock() {
     const getCurrentTime = () => {
@@ -37,3 +48,7 @@ startClock()
 date.textContent = new Date().getDate() + '.' + (new Date().getMonth() + 1) + '.' +new Date().getFullYear()
 
 
+document.querySelector('#searchbar').addEventListener('keydown', e => {
+    if (e.key != "Enter") return
+    getData(e.target.value).then(res => useData(res))
+})
